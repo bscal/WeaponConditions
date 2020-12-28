c2b489d8f29433396ae3203133be55bda47cdf6c
Jon Fus <jonfus123@hotmail.com>
2020-12-28 00:46:23 -0500

    SharpenedCondition now uses LoreStats

--- src/main/java/me/bscal/conditions/SharpenedCondition.java
+++ src/main/java/me/bscal/conditions/SharpenedCondition.java
@@ -1,13 +1,13 @@
 package me.bscal.conditions;
 
+import me.bscal.WeaponConditions;
 import me.bscal.items.ItemConstants;
 import me.bscal.lore.ColorConstants;
-import me.bscal.utils.ItemUtils;
-import org.bukkit.attribute.Attribute;
+import me.bscal.lore.stats.Operation;
+import me.bscal.lore.stats.Stat;
 import org.bukkit.attribute.AttributeModifier;
 import org.bukkit.inventory.EquipmentSlot;
 import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.meta.ItemMeta;
 
 import java.util.UUID;
 
@@ -15,29 +15,36 @@ public class SharpenedCondition extends Condition
 {
 
 	private static final String NAME = "Sharpened";
-	private static final double BONUS_DMG = 4.0;
+	private static final float BONUS_DMG = 4.0f;
 	private static final AttributeModifier MODIFIER = new AttributeModifier(UUID.randomUUID(), NAME,
 			BONUS_DMG, AttributeModifier.Operation.ADD_NUMBER, EquipmentSlot.HAND);
+
+	private final WeaponConditions m_plugin;
+	private final Stat m_damageStat;
 
 	public SharpenedCondition()
 	{
 		super(NAME, ColorConstants.GOOD, (ItemStack[]) ItemConstants.WEAPONS);
+		m_plugin = WeaponConditions.Get();
+		m_damageStat = m_plugin.GetLoreStats().GetStat("Damage");
 	}
 
 	@Override
 	public void Apply(ItemStack item)
 	{
-		ItemUtils.AddAttribute(item, Attribute.GENERIC_ATTACK_DAMAGE, MODIFIER, true);
+		//ItemUtils.AddAttribute(item, Attribute.GENERIC_ATTACK_DAMAGE, MODIFIER, true);
+		m_plugin.GetLoreStats()
+				.AddStatToItem(item, m_damageStat.CreateContainer(BONUS_DMG, Operation.ADD));
 	}
 
 	@Override
 	public void Remove(ItemStack item)
 	{
-		ItemMeta im = item.getItemMeta();
-
-		im.removeAttributeModifier(Attribute.GENERIC_ATTACK_DAMAGE, MODIFIER);
-
-		item.setItemMeta(im);
+		m_plugin.GetLoreStats()
+				.RemoveStatFromItem(item, m_damageStat.CreateContainer(-BONUS_DMG, Operation.ADD));
+		//ItemMeta im = item.getItemMeta();
+		//im.removeAttributeModifier(Attribute.GENERIC_ATTACK_DAMAGE, MODIFIER);
+		//item.setItemMeta(im);
 	}
 
 	@Override
